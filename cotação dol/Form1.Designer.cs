a4efd4d30ef56d7a30e976399a40a5e960c1ffef
Henrique Vieira de Jesus <henriquerkn@gmail.com>
2023-04-19 19:56:48 -0300

    converção feita

--- cotação dol/Form1.Designer.cs
+++ cotação dol/Form1.Designer.cs
@@ -28,20 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonConverter = new System.Windows.Forms.Button();
+            this.textBoxReal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDolar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonLimpar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(515, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escreva em Reais para ver o resultado em Dólares";
+            // 
+            // buttonConverter
+            // 
+            this.buttonConverter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonConverter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonConverter.Font = new System.Drawing.Font("Schadow BT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConverter.Location = new System.Drawing.Point(154, 122);
+            this.buttonConverter.Name = "buttonConverter";
+            this.buttonConverter.Size = new System.Drawing.Size(118, 31);
+            this.buttonConverter.TabIndex = 1;
+            this.buttonConverter.Text = "Converter";
+            this.buttonConverter.UseVisualStyleBackColor = false;
+            this.buttonConverter.Click += new System.EventHandler(this.buttonConverter_Click);
+            // 
+            // textBoxReal
+            // 
+            this.textBoxReal.BackColor = System.Drawing.Color.Black;
+            this.textBoxReal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxReal.ForeColor = System.Drawing.Color.White;
+            this.textBoxReal.Location = new System.Drawing.Point(65, 83);
+            this.textBoxReal.Name = "textBoxReal";
+            this.textBoxReal.Size = new System.Drawing.Size(96, 20);
+            this.textBoxReal.TabIndex = 2;
+            this.textBoxReal.TextChanged += new System.EventHandler(this.textBoxReal_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(32, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "R$";
+            // 
+            // textBoxDolar
+            // 
+            this.textBoxDolar.BackColor = System.Drawing.Color.Black;
+            this.textBoxDolar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxDolar.ForeColor = System.Drawing.Color.White;
+            this.textBoxDolar.Location = new System.Drawing.Point(346, 83);
+            this.textBoxDolar.Name = "textBoxDolar";
+            this.textBoxDolar.Size = new System.Drawing.Size(96, 20);
+            this.textBoxDolar.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(284, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dolar $";
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.buttonLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLimpar.Font = new System.Drawing.Font("Schadow BT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLimpar.Location = new System.Drawing.Point(324, 122);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(118, 31);
+            this.buttonLimpar.TabIndex = 6;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = false;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(956, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(543, 250);
+            this.Controls.Add(this.buttonLimpar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxDolar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxReal);
+            this.Controls.Add(this.buttonConverter);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Conversão";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonConverter;
+        private System.Windows.Forms.TextBox textBoxReal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDolar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonLimpar;
     }
 }
 
--- cotação dol/Form1.cs
+++ cotação dol/Form1.cs
@@ -12,9 +12,31 @@ namespace cotação_dol
 {
     public partial class Form1 : Form
     {
+  
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBoxReal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConverter_Click(object sender, EventArgs e)
+        {
+            textBoxDolar.Text = (Convert.ToDouble(textBoxReal.Text) * 4.91).ToString();
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            textBoxDolar.Clear();
+            textBoxReal.Clear();
         }
     }
 }
